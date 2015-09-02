8fc23ebe6e66866d7ec2a388286307b5a17b83f2
Chris Meyers <chris.meyers.fsu@gmail.com>
2015-09-01 21:08:56 -0400

    pypi setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(
+    name='sitecustomize',
+    version='1.0.0',
+    url='https://github.com/chrismeyersfsu/sitecustomize',
+    author='Chris Meyers',
+    author_email='cmeyers@ansible.com',
+    keywords='environment addsitedir',
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(),
+    install_requires=[],
+)
