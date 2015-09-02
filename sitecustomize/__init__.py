a1cc6350577822fa6d9bdd2b13631ed3b9533dc8
Chris Meyers <chris.meyers.fsu@gmail.com>
2015-09-02 08:09:28 -0400

    correct path

--- sitecustomize/__init__.py
+++ sitecustomize/__init__.py
@@ -5,7 +5,8 @@ import os
 import sys
 import site
 
-site.addsitedir(os.path.dirname(os.path.join(os.path.realpath(__file__), "..")))
+sitedir = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+site.addsitedir(sitedir)
 # Work around https://bugs.python.org/issue7744
 # by moving  local_site_packages to the front of sys.path
 sys.path.insert(0, sys.path.pop())
